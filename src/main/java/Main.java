1342ea5dcbfa99084c3ea27e2d22e6596d7b0366
Ben Donaghy <bendonaghy321@gmail.com>
2022-01-12 06:32:32 +0000

    creating git repo

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student("John", 'B', 2);
+
+        student.upGrade();
+        System.out.println(student.displayGrade());
+        student.upGrade();
+
+        student.downGrade();
+        student.downGrade();
+        student.downGrade();
+        student.downGrade();
+        System.out.println(student.displayGrade());
+
+        System.out.println(student.displayName());
+
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+
+    private String name;
+    private char grade;
+    private int group;
+    private final String SECRETNICKNAME = "MySecretNickName";
+
+    public Student(String name, char grade, int group) {
+        this.name = name;
+        this.grade = grade;
+        this.group = group;
+    }
+
+    public char displayGrade() {
+        return grade;
+    }
+
+    public String displayName() {
+        return name;
+    }
+    
+    public void upGrade() {
+        if (Character.toString(grade).equals("A")) {
+            System.out.println("The grade is already the highest possible!");
+        } else {
+            grade -= 1;
+        }
+    }
+
+    public void downGrade() {
+        if (Character.toString(grade).equals("D")) {
+            System.out.println("The grade is already the lowest possible!");
+        } else {
+            grade += 1;
+        }
+    }
+
+}
